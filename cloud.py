1b41a368cc22b4a24b5032010b8518235c934622
cesarcruz <cesar@logicsp.com.br>
2017-09-11 10:25:47 -0300

    geo distance

--- cloud.py
+++ cloud.py
@@ -3,34 +3,33 @@ import urllib.request, json
 
 
 def geo( coords_1, coords_2 ):
- url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=-22.3757058,-47.3937795&destinations=-22.5827088,-47.400379699999995&key=%20AIzaSyAhpEf5XIFeMDdPYvembz7WWWh5ryRTO-0"
+ url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={0},{1}&destinations={2},{3}&key=%20AIzaSyAhpEf5XIFeMDdPYvembz7WWWh5ryRTO-0".format(coords_1[0], coords_1[1], coords_2[0], coords_2[1])
  with urllib.request.urlopen(url) as url:
     result = json.loads(url.read().decode())
 
+ origin_addresses = result['origin_addresses'][0]
+ destination_addresses = result['destination_addresses'][0]
  driving_time = result['rows'][0]['elements'][0]['duration']['text']
  distance_time = result['rows'][0]['elements'][0]['distance']['text']
- print("distance: {0} | driving time: {1}".format(distance_time, driving_time))
+ print("Endereço origem: {0}".format(origin_addresses))
+ print("Endereço destino: {0}".format(destination_addresses))
+ print("Distância: {0}".format(distance_time))
+ print("Tempo de Carro: {0}".format(driving_time))
+ print("\n")
 
  return result
 
-# The callback for when the client successfully connects to the broker
 def on_connect(client, userdata, flags, rc):
-    ''' We subscribe on_connect() so that if we lose the connection
-        and reconnect, subscriptions will be renewed. '''
-
     client.subscribe("owntracks/+/+")
 
-# The callback for when a PUBLISH message is received from the broker
 def on_message(client, userdata, msg):
 
     topic = msg.topic
 
     try:
         data = json.loads(str(msg.payload, 'ascii'))
-        print(data)
-        print("TID = {0} is currently at {1}, {2}".format(data['tid'], data['lat'], data['lon']))
         coords_1 = (data['lat'], data['lon'])
-        coords_2 = (-22.5827088, -47.400379699999995)
+        coords_2 = (-22.375454, -47.393242)
         geo(coords_1, coords_2)
     except Exception as e:
         print("Error: {0}".format(e))
@@ -40,8 +39,5 @@ client.on_connect = on_connect
 client.on_message = on_message
 client.username_pw_set(username="ha", password="133906949")
 client.connect("m11.cloudmqtt.com", 12821, 60)
-
-# Blocking call which processes all network traffic and dispatches
-# callbacks (see on_*() above). It also handles reconnecting.
 
 client.loop_forever()
